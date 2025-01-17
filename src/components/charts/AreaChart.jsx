d60b1fd36b6c9c06be0f16504a2438d289414b05
FollowNaim <naimxf@gmail.com>
2025-01-17 17:09:15 +0600

    feat(fix): area chart legend issue finally
    Signed-off-by: FollowNaim <naimxf@gmail.com>

--- src/components/charts/AreaChart.jsx
+++ src/components/charts/AreaChart.jsx
@@ -1,5 +1,3 @@
-import { Area, AreaChart, CartesianGrid, XAxis } from "recharts";
-
 import {
   Card,
   CardContent,
@@ -10,21 +8,23 @@ import {
 import {
   ChartContainer,
   ChartLegend,
-  ChartLegendContent,
   ChartTooltip,
   ChartTooltipContent,
 } from "@/components/ui/chart";
-
+import {
+  Area,
+  AreaChart,
+  CartesianGrid,
+  ResponsiveContainer,
+  XAxis,
+} from "recharts";
 const chartConfig = {
-  visitors: {
-    label: "Visitors",
-  },
   desktop: {
-    label: "Desktop",
+    label: "requested",
     color: "hsl(var(--chart-1))",
   },
   mobile: {
-    label: "Mobile",
+    label: "rejected",
     color: "hsl(var(--chart-2))",
   },
 };
@@ -35,20 +35,17 @@ export function AreaChartCustom({
   rejectedTotal,
   rejectedUnpaid,
 }) {
-  const filteredData = rejectedUnpaid.filter((item) => {
-    return { x: 0 };
-  });
-  console.log(filteredData);
+  console.log(rejectedUnpaid);
   return (
     <Card>
       <CardHeader className="flex items-center gap-2 space-y-0 border-b py-5 sm:flex-row">
         <div className="grid flex-1 gap-1 text-center sm:text-left">
           <CardTitle>
-            Unpaid Sales Overview - (${inTotal || 0}) - Rejected Sales ($
-            {rejectedTotal || 0})
+            Total Unpaid Sales - (${inTotal}) And Rejected Sales - ($
+            {rejectedTotal})
           </CardTitle>
           <CardDescription>
-            A summary of all-time unpaid sales revenue
+            Showing total visitors for the last 3 months
           </CardDescription>
         </div>
       </CardHeader>
@@ -57,59 +54,95 @@ export function AreaChartCustom({
           config={chartConfig}
           className="aspect-auto h-[250px] w-full"
         >
-          <AreaChart data={rejectedUnpaid}>
-            <defs>
-              <linearGradient id="fillDesktop" x1="0" y1="0" x2="0" y2="1">
-                <stop
-                  offset="5%"
-                  stopColor="var(--color-desktop)"
-                  stopOpacity={0.8}
-                />
-                <stop
-                  offset="95%"
-                  stopColor="var(--color-desktop)"
-                  stopOpacity={0.1}
-                />
-              </linearGradient>
-              <linearGradient id="fillMobile" x1="0" y1="0" x2="0" y2="1">
-                <stop
-                  offset="5%"
-                  stopColor="var(--color-mobile)"
-                  stopOpacity={0.8}
-                />
-                <stop
-                  offset="95%"
-                  stopColor="var(--color-mobile)"
-                  stopOpacity={0.1}
-                />
-              </linearGradient>
-            </defs>
-            <CartesianGrid vertical={false} />
-            <XAxis
-              dataKey="_id"
-              tickLine={false}
-              axisLine={false}
-              tickMargin={8}
-              minTickGap={32}
-              tickFormatter={(value) => value.slice(0, 3)}
-            />
-            <ChartTooltip cursor={false} content={<ChartTooltipContent />} />
-            <Area
-              dataKey="occur"
-              type="natural"
-              fill="url(#fillDesktop)"
-              stroke="var(--color-desktop)"
-              stackId="a"
-            />
-            <Area
-              dataKey="totalPrice"
-              type="natural"
-              fill="url(#fillMobile)"
-              stroke="var(--color-Mobile)"
-              stackId="btt"
-            />
-            <ChartLegend content={<ChartLegendContent />} />
-          </AreaChart>
+          <ResponsiveContainer width={"100%"} height={"100%"}>
+            <AreaChart data={rejectedUnpaid}>
+              <defs>
+                <linearGradient id="fillDesktop" x1="0" y1="0" x2="0" y2="1">
+                  <stop
+                    offset="5%"
+                    stopColor="var(--color-desktop)"
+                    stopOpacity={0.8}
+                  />
+                  <stop
+                    offset="95%"
+                    stopColor="var(--color-desktop)"
+                    stopOpacity={0.1}
+                  />
+                </linearGradient>
+                <linearGradient id="fillMobile" x1="0" y1="0" x2="0" y2="1">
+                  <stop
+                    offset="5%"
+                    stopColor="var(--color-mobile)"
+                    stopOpacity={0.8}
+                  />
+                  <stop
+                    offset="95%"
+                    stopColor="var(--color-mobile)"
+                    stopOpacity={0.1}
+                  />
+                </linearGradient>
+              </defs>
+              <CartesianGrid vertical={false} />
+              <XAxis
+                dataKey="_id"
+                tickLine={false}
+                axisLine={false}
+                tickMargin={8}
+                minTickGap={32}
+                label={"Total Unpaid And Rejected Statics"}
+              />
+
+              <ChartTooltip
+                cursor={false}
+                content={<ChartTooltipContent indicator="dot" />}
+              />
+              <Area
+                dataKey="products"
+                type="natural"
+                fill="url(#fillMobile)"
+                stroke="var(--color-mobile)"
+                stackId="a"
+              />
+              <Area
+                dataKey="totalPrice"
+                type="natural"
+                fill="url(#fillDesktop)"
+                stroke="var(--color-desktop)"
+                stackId="a"
+              />
+              <ChartLegend
+                content={() => (
+                  <div className="flex gap-4 justify-center mt-4">
+                    <div className="flex items-center gap-2">
+                      <span
+                        style={{
+                          backgroundColor: chartConfig.desktop.color,
+                          display: "inline-block",
+                          width: 12,
+                          height: 12,
+                          borderRadius: "10%",
+                        }}
+                      ></span>
+                      <span>{chartConfig.desktop.label}</span>
+                    </div>
+
+                    <div className="flex items-center gap-2">
+                      <span
+                        style={{
+                          backgroundColor: chartConfig.mobile.color,
+                          display: "inline-block",
+                          width: 12,
+                          height: 12,
+                          borderRadius: "10%",
+                        }}
+                      ></span>
+                      <span>{chartConfig.mobile.label}</span>
+                    </div>
+                  </div>
+                )}
+              />
+            </AreaChart>
+          </ResponsiveContainer>
         </ChartContainer>
       </CardContent>
     </Card>
